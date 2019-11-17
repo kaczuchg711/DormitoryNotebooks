9e12503010b4a9e49320e1d07745145874d0cfe0
Tomasz <kaczatom4@gmail.com>
2019-11-17 16:47:05 +0100

    first JS php work

---
+++ index.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+
+	<?php
+		echo "jestem php<br>"
+	?>
+	<script language="JavaScript" >
+		document.write("jestem js<br>")
+	</script>
+	jestem html
+</body>
+</html>
+
+
+
