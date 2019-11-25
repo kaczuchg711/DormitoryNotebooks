dc2a9c8bb9fc6118ea418d6f3f8945775f176e76
Tomasz <kaczatom4@gmail.com>
2019-11-25 23:57:26 +0100

    fight with buttons

--- Views/DefaultController/home.php
+++ Views/DefaultController/home.php
@@ -4,22 +4,21 @@
 	<link rel="Stylesheet" type="text/css" href="Public/css/bootstrap-4.3.1-dist/css/bootstrap.css"/>
 	<link rel="Stylesheet" type="text/css" href="Public/css/bootstrap-4.3.1-dist/css/bootstrap-grid.css"/>
 	<link rel="Stylesheet" type="text/css" href="Public/css/bootstrap-4.3.1-dist/css/bootstrap-reboot.css"/>
-	<link rel="Stylesheet" type="text/css" href="Public/css/main.css"/>
+	<link rel="Stylesheet" type="text/css" href="Public/css/style.css"/>
 	<meta charset="UTF-8">
 	<!--		<meta http-equiv="refresh" content="1">-->
 	<title>Akademikowe Zeszyty</title>
+	<meta http-equiv="refresh" content="1">
 </head>
 <body>
 	<h2>Witaj<br>Co chcesz zrobić ?</h2>
 
-
-	<button>Skorzystać z pralni</button>
-	<button>Zgłosić usterkę</button>
-	<button>Wynająć salę z bilardem</button>
-	<button>Wypożyczyć odkurzacz</button>
-	<button>Wypożyczyć suszarkę</button>
-
-
+	<div class="button_container">
+		<button class="home_button">Skorzystać z pralni</button>
+		<button class="home_button">Zgłosić usterkę</button>
+		<button class="home_button">Wynająć salę z bilardem</button>
+		<button class="home_button">Wypożyczyć odkurzacz</button>
+		<button class="home_button">Wypożyczyć suszarkę</button>
 	</div>
 
 </body>
